470bc90704f92d8d2f99dda2221633696645784c
DeveloperJosh <bam0909@outlook.com>
2024-06-30 15:55:08 -0400

    feat: Add meta tags for improved SEO and user experience

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#01a0e9" />
+        <meta name="description" content="NekoNode the best Anime CLI/Site there is." />
+        <title>NekoNode</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
