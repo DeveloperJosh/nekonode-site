17b2add847b5ed523fc313320887dc43fac6ec5b
DeveloperJosh <bam0909@outlook.com>
2024-06-30 15:59:52 -0400

    chore: Update meta tags and add environment variable for API URL

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,30 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    document.body.classList.add('scrollbar-thumb-rounded', 'scrollbar-thin', 'scrollbar-thumb-yellow-500', 'scrollbar-track-gray-700');
+    document.body.classList.add(
+      'scrollbar-thumb-rounded',
+      'scrollbar-thin',
+      'scrollbar-thumb-yellow-500',
+      'scrollbar-track-gray-700'
+    );
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#01a0e9" />
+        <meta name="description" content="NekoNode the best Anime CLI/Site there is" />
+        <meta property="og:title" content="NekoNode" />
+        <meta property="og:description" content="NekoNode the best Anime CLI/Site there is" />
+        <title>NekoNode</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#01a0e9" />
-        <meta name="description" content="NekoNode the best Anime CLI/Site there is." />
-        <title>NekoNode</title>
       </Head>
       <body>
         <Main />
