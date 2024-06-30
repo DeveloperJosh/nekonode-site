d946e6f71649f98ece490b5a88ba3ac232a79855
DeveloperJosh <bam0909@outlook.com>
2024-06-30 03:26:55 -0400

    refactor: Remove ESLint configuration and update image rendering in AnimeCard component

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const AnimeCard = ({ anime }) => {
   const animeId = anime.encodedName;
   return (
     <Link href={`/anime/${encodeURIComponent(animeId)}`} legacyBehavior>
       <a className="block border border-gray-300 p-4 m-4 text-center rounded-lg">
-        <img src={anime.image} alt={anime.name} className="w-full h-64 object-cover rounded-lg" />
+        <Image src={anime.image} alt={anime.name} className="w-full h-64 object-cover rounded-lg" />
         <h3 className="text-lg font-semibold mt-2">{anime.name}</h3>
         <p className="text-gray-500">{anime.lang}</p>
       </a>
