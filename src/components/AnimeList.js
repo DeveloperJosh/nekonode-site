5cd6d30b39ebdb0bff75e07290def052a39a99d3
DeveloperJosh <bam0909@outlook.com>
2024-06-30 17:42:22 -0400

    new css, I might redo the whole UI

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 const AnimeList = ({ animes }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
       {animes.map((anime) => (
-        <div key={anime.encodedName} className="bg-gray-700 p-4 rounded-lg shadow-lg hover:bg-gray-600 text-center transform transition-transform duration-200 hover:scale-105">
-          <div className="relative w-full pb-[150%] mb-4">
-            <Image
-              src={anime.image}
-              alt={anime.name}
-              fill
-              sizes='200px'
-              className="rounded-lg object-contain"
-            />
-          </div>
-          <h2 className="text-xl font-bold text-yellow-500 mb-2">{anime.name}</h2>
-          <Link href={`/anime/${anime.encodedName}`} legacyBehavior>
-            <a className="mt-2 bg-yellow-500 text-gray-800 px-4 py-2 rounded hover:bg-yellow-600 inline-block">View Details</a>
-          </Link>
-        </div>
+        <Link href={`/anime/${anime.encodedName}`} key={anime.id} legacyBehavior>
+          <a className="anime-card block bg-gray-700 p-4 rounded-lg shadow hover:bg-gray-600 text-center">
+            <div className="w-full h-96 overflow-hidden rounded-lg mb-2">
+              <img src={anime.image} alt={anime.encodedName} className="w-full h-full object-cover" />
+            </div>
+            <h3 className="text-lg font-bold text-gray-300 truncate">{anime.name}</h3>
+          </a>
+        </Link>
       ))}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,30 @@ import AnimeList from '../components/AnimeList';
 const api = "https://api.nekonode.net";
 
 const HomePage = ({ latestAnime, page }) => (
-  <div className="flex-grow">
-    <div className="bg-gray-800 p-6 rounded-lg shadow-lg mt-4 overflow-auto scrollbar-thumb-rounded scrollbar-thin scrollbar-thumb-yellow-500 scrollbar-track-gray-700">
-      <div className="text-center mb-4 flex justify-between items-center">
-        {page > 1 && (
-          <a
-            href={`/?page=${page - 1}`}
-            className="bg-yellow-500 text-gray-800 px-4 py-2 rounded hover:bg-yellow-600"
-          >
-            Previous Page
-          </a>
-        )}
-        <a
-          href={`/?page=${page + 1}`}
-          className="bg-yellow-500 text-gray-800 px-4 py-2 rounded hover:bg-yellow-600"
-        >
-          Next Page
-        </a>
+  <div className="bg-gray-900 min-h-screen text-gray-200">
+    <div className="container mx-auto px-4 py-8">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+        <div className="flex items-center justify-between mb-4">
+          <h1 className="text-4xl font-bold text-yellow-500">Recently Updated</h1>
+          <div className="flex space-x-2">
+            {page > 1 && (
+              <a
+                href={`/?page=${page - 1}`}
+                className="bg-yellow-500 text-gray-800 px-4 py-2 rounded hover:bg-yellow-600"
+              >
+                Previous Page
+              </a>
+            )}
+            <a
+              href={`/?page=${page + 1}`}
+              className="bg-yellow-500 text-gray-800 px-4 py-2 rounded hover:bg-yellow-600"
+            >
+              Next Page
+            </a>
+          </div>
+        </div>
+        <AnimeList animes={latestAnime} />
       </div>
-      <h1 className="text-4xl font-bold text-center mb-4 text-yellow-500">Latest Anime</h1>
-      <AnimeList animes={latestAnime} />
     </div>
   </div>
 );
