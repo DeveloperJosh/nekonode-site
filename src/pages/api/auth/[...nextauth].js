28a0ea14950a9341dcec51771a856e5bfe0972f0
DeveloperJosh <bam0909@outlook.com>
2024-07-04 01:47:33 -0400

    chore: Update NextAuth configuration and add Discord account access token

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,9 +2,6 @@ import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 import dbConnect from '../../../lib/dbConnect';
 import User from '../../../models/User';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const authOptions = {
   providers: [
@@ -12,7 +9,6 @@ export const authOptions = {
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
-
   ],
   callbacks: {
     async session({ session, token }) {
@@ -20,32 +16,36 @@ export const authOptions = {
       session.accessToken = token.accessToken;
       return session;
     },
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
       if (user) {
         token.id = user.id;
-        token.accessToken = user.accessToken;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
       }
       return token;
     },
-    async signIn({ user }) {
+    async signIn({ user, account, profile }) {
       await dbConnect();
-      
+
       const existingUser = await User.findOne({ email: user.email });
-      
+
       if (!existingUser) {
         await User.create({
-          _id: user.id,
-          userId: user.id,
           name: user.name,
           email: user.email,
           image: user.image,
         });
       }
-      
+
       return true;
     },
   },
-  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/auth/signin',  // Custom sign-in page URL
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === 'development',
 };
 
 export default NextAuth(authOptions);
