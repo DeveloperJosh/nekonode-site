2a51f77d99a75c76b7078c4da9df7f9a61433f9c
DeveloperJosh <bam0909@outlook.com>
2024-07-10 21:03:56 -0400

    this made me so mad

--- src/pages/auth/profile.js
+++ src/pages/auth/profile.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import UpdateToList from '@/components/Updatetolist';
+import UpdateToList from '@/components/UpdateToList';
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
